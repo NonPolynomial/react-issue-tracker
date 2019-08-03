95ef9c4c411f7dfc213affae659b3addba3a76cf
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-03 16:22:17 +0200

    - add hooks to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 import Projects from './services/Projects';
 
@@ -8,55 +6,53 @@ import Layout from './components/Layout';
 import ProjectOverview from './components/ProjectOverview';
 import ProjectView from './components/ProjectView';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import './App.css';
 
-    this.state = {
-      projects: [],
-      dataFetched: false,
-      selectedProject: null,
+const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [fetchStatus, updateFetchStatus] = useState('unsent');
+  const [currentProject, selectProject] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const result = await Projects.fetchData(500);
+      setProjects(result);
+      updateFetchStatus('done');
     };
-  }
 
-  async componentDidMount() {
-    const projects = await Projects.fetchData(500);
-    this.setState({ projects, dataFetched: true });
-  }
-
-  render() {
-    const { projects, dataFetched, selectedProject } = this.state;
-
-    if (!dataFetched) {
-      return (
-        <Layout>
-          <p>Fetching data...</p>
-        </Layout>
-      );
+    if (fetchStatus === 'unsent') {
+      fetchProjects();
+      updateFetchStatus('fetching');
     }
+  }, [fetchStatus]);
 
-    let content = <p>No projects found.</p>;
-
-    if (selectedProject) {
-      content = (
-        <ProjectView
-          project={selectedProject}
-          onDeselect={() => this.setState({ selectedProject: null })}
-        />
-      );
-    } else if (projects.length > 0) {
-      content = (
-        <ProjectOverview
-          projects={projects}
-          onProjectSelect={project =>
-            this.setState({ selectedProject: project })
-          }
-        />
-      );
-    }
-
-    return <Layout>{content}</Layout>;
+  if (fetchStatus !== 'done') {
+    return (
+      <Layout>
+        <p>Fetching data...</p>
+      </Layout>
+    );
   }
-}
+
+  let content = <p>No projects found.</p>;
+
+  if (currentProject) {
+    content = (
+      <ProjectView
+        project={currentProject}
+        onDeselect={() => selectProject(null)}
+      />
+    );
+  } else if (projects.length > 0) {
+    content = (
+      <ProjectOverview
+        projects={projects}
+        onProjectSelect={project => selectProject(project)}
+      />
+    );
+  }
+
+  return <Layout>{content}</Layout>;
+};
 
 export default App;
