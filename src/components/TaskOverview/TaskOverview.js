44a21fcc40373ba82f7dfa552d8cc8acbadb9bab
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-03 15:09:35 +0200

    - half way done on TaskOverview

--- src/components/TaskOverview/TaskOverview.js
+++ src/components/TaskOverview/TaskOverview.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import { Task } from '../../types';
 
 import './TaskOverview.css';
 
-import { Task } from '../../types';
+const getRatio = (partial, total) => Number((partial / total) * 100).toFixed(2);
 
 const propTypes = {
   tasks: PropTypes.arrayOf(Task).isRequired,
@@ -14,95 +16,97 @@ const defaultProps = {
   onTaskSelect: Function.prototype,
 };
 
-class TaskOverview extends React.Component {
-  constructor(props) {
-    super(props);
+const TaskOverview = ({ tasks, onTaskSelect }) => {
+  const total = tasks.length;
 
-    this.state = {
-      todo: props.tasks,
-      progress: [],
-      done: [],
-      total: 0,
-    };
-  }
+  const [todoTasks, setTodoTasks] = useState(tasks);
+  const [progressTasks, setProgressTasks] = useState(tasks);
+  const [doneTasks, setDoneTasks] = useState(tasks);
 
-  componentDidMount() {
-    this.categorizeTasks();
-  }
+  useEffect(() => {
+    console.log('effect');
+    setTodoTasks(tasks.filter(({ status }) => status === 'todo'));
+    setProgressTasks(tasks.filter(({ status }) => status === 'progress'));
+    setDoneTasks(tasks.filter(({ status }) => status === 'done'));
+  });
 
-  categorizeTasks() {
-    this.setState({
-      todo: this.props.tasks.filter(({ status }) => status === 'todo'),
-      progress: this.props.tasks.filter(({ status }) => status === 'progress'),
-      done: this.props.tasks.filter(({ status }) => status === 'done'),
-      total: this.props.tasks.length,
-    });
-  }
-
-  moveTask(task, status) {
-    task.status = status;
-    this.categorizeTasks();
-  }
-
-  static getRatio(partial, total) {
-    return Number((partial / total) * 100).toFixed(2);
-  }
-
-  render() {
-    const { onTaskSelect } = this.props;
-    const { todo, progress, done, total } = this.state;
-    return (
-      <div className="task-overview__container">
-        <div className="task-overview_column">
-          <h4>
-            Todo ({todo.length} | {TaskOverview.getRatio(todo.length, total)}%)
-          </h4>
-          <div className="task-overview__tasks">
-            {todo.map(task => (
-              <article key={task.id} className="task-overview_item">
-                <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
-                <button onClick={() => this.moveTask(task, 'progress')}>
-                  ▶
-                </button>
-                <button onClick={() => this.moveTask(task, 'done')}>✔</button>
-              </article>
-            ))}
-          </div>
-        </div>
-        <div className="task-overview_column">
-          <h4>
-            In Progress ({progress.length} |{' '}
-            {TaskOverview.getRatio(progress.length, total)}%)
-          </h4>
-          <div className="task-overview__tasks">
-            {progress.map(task => (
-              <article key={task.id} className="task-overview_item">
-                <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
-                <button onClick={() => this.moveTask(task, 'todo')}>◀</button>
-                <button onClick={() => this.moveTask(task, 'done')}>✔</button>
-              </article>
-            ))}
-          </div>
-        </div>
-        <div className="task-overview_column">
-          <h4>
-            Done ({done.length} | {TaskOverview.getRatio(done.length, total)}%)
-          </h4>
-          <div className="task-overview__tasks">
-            {done.map(task => (
-              <article key={task.id} className="task-overview_item">
-                <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
-                <button onClick={() => this.moveTask(task, 'progress')}>
-                  ◀
-                </button>
-              </article>
-            ))}
-          </div>
+  return (
+    <div className="task-overview__container">
+      <div className="task-overview_column">
+        <h4>
+          Todo ({todoTasks.length} | {getRatio(todoTasks.length, total)}%)
+        </h4>
+        <div className="task-overview__tasks">
+          {todoTasks.map(task => (
+            <article key={task.id} className="task-overview_item">
+              <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
+              <button
+                onClick={() => {
+                  task.status = 'progress';
+                }}
+              >
+                ▶
+              </button>
+              <button
+                onClick={() => {
+                  task.status = 'done';
+                }}
+              >
+                ✔
+              </button>
+            </article>
+          ))}
         </div>
       </div>
-    );
-  }
-}
+      <div className="task-overview_column">
+        <h4>
+          In Progress ({progressTasks.length} |{' '}
+          {getRatio(progressTasks.length, total)}%)
+        </h4>
+        <div className="task-overview__tasks">
+          {progressTasks.map(task => (
+            <article key={task.id} className="task-overview_item">
+              <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
+              <button
+                onClick={() => {
+                  task.status = 'todo';
+                }}
+              >
+                ◀
+              </button>
+              <button
+                onClick={() => {
+                  task.status = 'done';
+                }}
+              >
+                ✔
+              </button>
+            </article>
+          ))}
+        </div>
+      </div>
+      <div className="task-overview_column">
+        <h4>
+          Done ({doneTasks.length} | {getRatio(doneTasks.length, total)}%)
+        </h4>
+        <div className="task-overview__tasks">
+          {doneTasks.map(task => (
+            <article key={task.id} className="task-overview_item">
+              <h4 onClick={() => onTaskSelect(task)}>{task.title}</h4>
+              <button
+                onClick={() => {
+                  task.status = 'progress';
+                }}
+              >
+                ◀
+              </button>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 TaskOverview.propTypes = propTypes;
 TaskOverview.defaultProps = defaultProps;
