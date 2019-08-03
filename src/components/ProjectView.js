993cf90ba797d2405c5cdaeecc63a68f2f8cc123
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-03 14:46:50 +0200

    - convert ProjectView to a functional components with hooks
    - add custom hook to force a component to update

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+import useForceUpdate from '../hooks/useForceUpdate';
+import { Project } from '../types';
 
 import TaskOverview from './TaskOverview';
 import TaskView from './TaskView';
-
-import { Project } from '../types';
 
 const propTypes = {
   project: Project.isRequired,
@@ -15,56 +16,47 @@ const defaultProps = {
   onDeselect: Function.prototype,
 };
 
-class ProjectView extends React.Component {
-  constructor(props) {
-    super(props);
+const ProjectView = ({ project, onDeselect }) => {
+  const forceUpdate = useForceUpdate();
+  const [currentTask, selectTask] = useState(null);
 
-    this.state = {
-      selectedTask: null,
-    };
-  }
-
-  render() {
-    const { project, onDeselect } = this.props;
-    const { selectedTask } = this.state;
-
-    let content;
-    if (selectedTask) {
-      content = (
-        <TaskView
-          task={selectedTask}
-          onStatusChange={(task, status) => {
-            task.status = status;
-            this.forceUpdate();
-          }}
-          onDeselect={() => this.setState({ selectedTask: null })}
-        />
-      );
-    } else {
-      content = (
-        <div>
-          <h3>Tasks</h3>
-          <TaskOverview
-            tasks={project.tasks}
-            onTaskSelect={task => this.setState({ selectedTask: task })}
-          />
-        </div>
-      );
-    }
-
-    return (
+  let content;
+  if (currentTask) {
+    content = (
+      <TaskView
+        task={currentTask}
+        onStatusChange={(task, status) => {
+          task.status = status;
+          forceUpdate();
+        }}
+        onDeselect={() => selectTask(null)}
+      />
+    );
+  } else {
+    content = (
       <div>
-        <button onClick={() => onDeselect()}>Back to overview</button>
-        <h2>Project: {project.title}</h2>
-        <h3>Summary</h3>
-        <p>{project.summary}</p>
-        <h3>Description</h3>
-        <p>{project.description}</p>
-        {content}
+        <h3>Tasks</h3>
+        <TaskOverview
+          tasks={project.tasks}
+          onTaskSelect={task => selectTask(task)}
+        />
+        ;
       </div>
     );
   }
-}
+
+  return (
+    <div>
+      <button onClick={() => onDeselect()}>Back to overview</button>
+      <h2>Project: {project.title}</h2>
+      <h3>Summary</h3>
+      <p>{project.summary}</p>
+      <h3>Description</h3>
+      <p>{project.description}</p>
+      {content}
+    </div>
+  );
+};
 
 ProjectView.propTypes = propTypes;
 ProjectView.defaultProps = defaultProps;
---
+++ src/hooks/useForceUpdate.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+const useForceUpdate = () => {
+  const [state, setState] = useState(false);
+
+  return () => setState(!state);
+};
+
+export default useForceUpdate;
